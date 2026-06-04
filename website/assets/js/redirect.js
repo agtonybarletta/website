099180c6f697809a3ff2c36d18b8294be2118aec
A.G. Tony Barletta <agtonybarletta@gmail.com>
2026-06-04 18:42:38 +0200

    fix health url

--- website/assets/js/redirect.js
+++ website/assets/js/redirect.js
@@ -1,10 +1,10 @@
 console.log("Redirecting to the homepage...");
 
-fetch("https://www.agtonybarletta.com/health").then(
+fetch("http://www.agtonybarletta.com/health").then(
     response => {
         if (response.ok) {
             console.log("The website is healthy. Redirecting to the homepage...");
-            window.location.href = "https://www.agtonybarletta.com";
+            window.location.href = "http://www.agtonybarletta.com";
         } else {
             console.error("The website is not healthy. Staying on the current page.");
         }
