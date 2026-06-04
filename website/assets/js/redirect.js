6b4ee74b694303d45c5af7a73d1dd14660e39a94
A.G. Tony Barletta <agtonybarletta@gmail.com>
2026-06-04 18:45:18 +0200

    fix health url, part 2, the revenge

--- website/assets/js/redirect.js
+++ website/assets/js/redirect.js
@@ -1,10 +1,10 @@
 console.log("Redirecting to the homepage...");
 
-fetch("http://www.agtonybarletta.com/health").then(
+fetch("http://www.agtonybarletta.it/health").then(
     response => {
         if (response.ok) {
             console.log("The website is healthy. Redirecting to the homepage...");
-            window.location.href = "http://www.agtonybarletta.com";
+            window.location.href = "http://www.agtonybarletta.it";
         } else {
             console.error("The website is not healthy. Staying on the current page.");
         }
