4fb9a0447d73ac634f15adb9a96198a49542b39a
A.G. Tony Barletta <agtonybarletta@gmail.com>
2026-06-05 18:50:41 +0200

    change selfhosted url and health url

--- website/assets/js/redirect.js
+++ website/assets/js/redirect.js
@@ -1,10 +1,10 @@
 console.log("Redirecting to the homepage...");
 
-fetch("http://www.agtonybarletta.it/health").then(
+fetch("https://website.agtonybarletta.it/health").then(
     response => {
         if (response.ok) {
             console.log("The website is healthy. Redirecting to the homepage...");
-            window.location.href = "http://www.agtonybarletta.it";
+            window.location.href = "https://website.agtonybarletta.it";
         } else {
             console.error("The website is not healthy. Staying on the current page.");
         }
